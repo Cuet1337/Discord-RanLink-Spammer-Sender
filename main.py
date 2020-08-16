32f016ee50e643d7ed573c8ed1d220b33d54c086
Cuet <66260320+Cuet1337@users.noreply.github.com>
2020-08-16 21:37:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+import discord 
+import time 
+import asyncio
+from discord.ext import commands
+
+prefix = '#'
+
+token = 'TOKEN-HERE'
+
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print(f'ON. Use {prefix}lol to start.')
+
+def getlinkFromFile():
+  with open("links.txt", 'r') as linkFile:
+    contents = linkFile.read().split("\n")
+    return contents[random.randint(0, len(contents))]
+
+@bot.command()
+async def send(ctx):
+    await ctx.message.delete()
+    await ctx.send(f'{getlinkFromFile()}')
+
+@bot.command()
+async def spammer(ctx):
+    await ctx.message.delete()
+    while True:
+        await ctx.send(f'{getlinkFromFile()}')
+
+bot.run(token, bot=False)
